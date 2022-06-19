d73c6e089d470840feec2718c5387d1280c61ede
Bestar <bestar1105@hotmail.com>
2022-06-19 11:09:45 +0900

    20220619

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -63,7 +63,7 @@
             <link rel="shortcut icon" href="<?php echo site_furl('/favicon_f.ico');?>">
         <?php break; 
         case APP_ORANGE :?>
-            <link rel="shortcut icon" href="<?php echo site_furl('/favicon_org.ico?v=3');?>">
+            <link rel="shortcut icon" href="<?php echo site_furl('/favicon_org.ico?v=4');?>">
         <?php break; 
         case APP_CT :?>
             <link rel="shortcut icon" href="<?php echo site_furl('/favicon_ct.ico?v=1');?>">
