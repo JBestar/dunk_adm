aa0f09d9d18108a1edcf9377617716df661cacef
jbestar <jbestar1105@gmail.com>
2022-08-04 01:45:26 +0900

    20220830

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -21,7 +21,7 @@
             <link rel="shortcut icon" href="<?php echo site_furl('/favicon_x.ico?v=3');?>">
         <?php break; 
         case APP_THUNDER :?>
-            <link rel="shortcut icon" href="<?php echo site_furl('/favicon_t.ico?v=2');?>">
+            <link rel="shortcut icon" href="<?php echo site_furl('/favicon_t.ico?v=3');?>">
         <?php break; 
         case APP_WORLD :?>
             <link rel="shortcut icon" href="<?php echo site_furl('/favicon_w.ico?v=1');?>">
