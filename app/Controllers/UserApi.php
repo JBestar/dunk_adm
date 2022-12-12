27fd92fbcaeb20bada855744aa0336e1d680393e
jbestar <jbestar1105@gmail.com>
2022-12-12 14:22:28 +0900

    20221212

--- app/Controllers/UserApi.php
+++ app/Controllers/UserApi.php
@@ -348,6 +348,8 @@ class UserApi extends BaseController
             }
             
             if ($bPermit) {
+        		writeLog("[assets] ".$strUid." (".$sess_id.")");
+
 				$this->modelSess->updateLast($sess_id);
                 if($objUser->mb_level < LEVEL_ADMIN)
                     $this->modelMember->calcTransfer($objUser);
