b08adb37ec55ee76920a7e651dd805c6b8a4d60a
jbestar <jbestar1105@gmail.com>
2023-05-17 11:07:25 +0900

    20230517

---
+++ public/assets/js/conf_follow-script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    addBtnEvent();
+});
+
+
+function addBtnEvent() {
+
+    $("#confsite-ok-btn-id").click(function() {
+
+        let follow_check = $("#confsite-follow-check-id").prop('checked') ? 1 : 0;
+        let follow_id = $("#confsite-follow-input-id").val().trim();
+        if(follow_check == 1 && follow_id.length < 1 ){
+            alert("따라가기 아이디를 입력해주세요");
+            return;
+        }
+
+        var jsonData = {follow_check:follow_check, follow_id:follow_id};
+
+        if (!confirm("저장하시겠습니까?"))
+            return;
+
+        jsonData = JSON.stringify(jsonData);
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: FURL + "/api/change_follow",
+            data: { json_: jsonData },
+            success: function(jResult) {
+                //console.log(jResult);
+                if (jResult.status == "success") {
+                    alert("저장되었습니다.");
+                } else if (jResult.status == "fail") {
+                    alert(jResult.msg);
+                }
+            },
+            error: function(request, status, error) {
+                // console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+            }
+
+        });
+    });
+
+
+    $("#confsite-cancel-btn-id").click(function() {
+        location.reload();
+    });
+
+}
