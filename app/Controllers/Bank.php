e9c84103103d3d7378e970b60b251525b415a5f2
jbestar <jbestar1105@gmail.com>
2024-05-06 11:02:21 +0900

    20240506-pragmt

--- app/Controllers/Bank.php
+++ app/Controllers/Bank.php
@@ -28,7 +28,11 @@ class Bank extends StdController {
 	}
 
 	public function exchange(){
-		$this->load_view_page('bank/exchange', 'bank_exchange');
+		$strUid = $this->session->user_id;
+		$objAdmin = $this->modelMember->getInfo($strUid);
+		if ($objAdmin != null && ($objAdmin->mb_level >= LEVEL_ADMIN || $_ENV['app.name'] != APP_DUNK) )
+			$this->load_view_page('bank/exchange', 'bank_exchange');
+		else $this->response->redirect( $_ENV['app.furl'].'/pages/login');
 	}
 
 	public function transfer(){
